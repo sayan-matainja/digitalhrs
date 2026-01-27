feb1382ec7c8870995b9f638b20a003b054b28cd
sayan-matainja <matainja0137@gmail.com>
2026-01-27 19:15:25 +0530

    others and change name on sidebar to "Designation"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminTableSeeder::class,
+            RoleTableSeeder::class,
             PermissionSeeder::class,
             CompanySeeder::class,
             AppSettingSeeder::class,
