e1a90e5f8ee5a3c57584554909275d9ed1518e99
sayan-matainja <matainja0137@gmail.com>
2026-02-26 18:36:29 +0530

    notification not hiding issue fixed

--- resources/views/admin/section/flash_message.blade.php
+++ resources/views/admin/section/flash_message.blade.php
@@ -9,41 +9,41 @@
 
 @if (Session::has('success'))
     <div style="max-width: 100%" id="message"   class="d-flex justify-content-between alert alert-success {{Session::has('success_important') ? 'alert-important': ''}} ">
-        
+
         @if(Session::has('success_important'))
         @endif
         {{session('success')}}
-        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-bs-dismiss="alert" aria-hidden="true">×</button>
     </div>
 @endif
 
 @if (Session::has('danger'))
     <div style="max-width: 100%" id="message" class="d-flex justify-content-between alert alert-danger {{Session::has('danger_important') ? 'alert-important': ''}}">
-        
+
         @if(Session::has('danger_important'))
         @endif
         {{session('danger')}}
-        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-bs-dismiss="alert" aria-hidden="true">×</button>
     </div>
 @endif
 
 @if (Session::has('info'))
     <div style="max-width: 100%" id="message" class="d-flex justify-content-between alert alert-info {{Session::has('info_important') ? 'alert-important': ''}}">
-        
+
         @if(Session::has('info_important'))
         @endif
         {{session('info')}}
-        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-bs-dismiss="alert" aria-hidden="true">×</button>
     </div>
 @endif
 
 @if (Session::has('warning'))
     <div style="max-width: 100%" id="message" class="d-flex justify-content-between alert alert-warning {{Session::has('warning_important') ? 'alert-important': ''}}">
-        
+
         @if(Session::has('warning_important'))
         @endif
         {{session('warning')}}
-        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-dismiss="alert" aria-hidden="true">×</button>
+        <button type="button" style="color:white !important;opacity: 1 !important;" class="close bg-dark border-0 rounded-1" data-bs-dismiss="alert" aria-hidden="true">×</button>
     </div>
 @endif
 <script>
